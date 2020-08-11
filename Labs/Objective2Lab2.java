275f0e9397f6faa5d7c16e198fb636f4e6f7117c
Tevon Patterson <Tevonpatt@gmail.com>
2020-08-11 12:40:30 -0700

    Objective2Lab2.class

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name;
+    name = "Tevon Patterson";
+    int age;
+    age = 24;
+    String state;
+    state = "Guam";
+    System.out.println("Name:" + " " + name + " " + "Age:" + " " + age + " " + "State:" + " " + state);
+  }
+}
