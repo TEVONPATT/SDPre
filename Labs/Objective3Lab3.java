db175bf41408d129377a42a196abb21d37719e5b
Tevon Patterson <Tevonpatt@gmail.com>
2020-08-11 18:23:41 -0700

    Objective3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,9 @@
+public class Objective3Lab3 {
+  public static void main(String[] args){
+    double num1 = 1 + (int)(Math.random() * 6);
+    double num2 = 1 + (int)(Math.random() * 6);
+    double num3 = 1 + (int)(Math.random() * 6);
+    double sum = (num1 + num2 + num3);
+    System.out.println(num1 + " " + "+" + " " + num2 + " " + "+" + " " + num3 + " " + "=" + " " + sum);
+  }
+}
