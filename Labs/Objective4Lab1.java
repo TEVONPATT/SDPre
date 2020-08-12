e768369096a2277a1849ce7a34028d8be7c4189c
Tevon Patterson <Tevonpatt@gmail.com>
2020-08-12 11:10:10 -0700

    Objective4Lab1.java

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("What is your first name?");
+    String fname = "John";
+    System.out.println("What is your last name?");
+    String lname = "Doe";
+    System.out.println("What is your favorite animal?");
+    String favoriteAnimal = "Lemur";
+    System.out.println("What is your favorite food?");
+    String favoriteFood = "Tacos";
+    System.out.println("What is your favorite song?");
+    String favoriteSong = "Twinkle Twinkle Little Star";
+    System.out.println("My name is" + " " + fname + " "+ lname + ".");
+    System.out.println("My favorite animal is the" + " " + favoriteAnimal + ".");
+    System.out.println("My favorite food is" + " " + favoriteFood + ".");
+    System.out.println("My favorite song is" + " " + favoriteSong + ".");
+  }
+}
