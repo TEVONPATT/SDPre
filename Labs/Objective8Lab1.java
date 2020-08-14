c4d4d9fdb4c614d971b1637c41e868e7ac9199bb
Tevon Patterson <Tevonpatt@gmail.com>
2020-08-14 15:29:23 -0700

    Objective8Lab1.java

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for(int i=1; i<=10; i+=1){
+      System.out.println(i);
+    }
+    for(int x=10; x>=1; x-=1){
+      System.out.println(x);
+    }
+  }
+}
