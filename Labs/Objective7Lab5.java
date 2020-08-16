28e4fa81ed86662f269b97c211d5dd678f9d6d5d
Tevon Patterson <Tevonpatt@gmail.com>
2020-08-15 18:49:46 -0700

    Objective7Lab5.java git commit -m Objective7Lab5.java git commit -m Objective7Lab5.java

---
+++ Labs/Objective7Lab5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Objective7Lab5{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+    //boolean bool;
+    while (true) {
+      System.out.println("___Menu___");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+      selection = scanner.nextInt();
+      if(selection == 1) {
+        System.out.println("Hello World");
+      }
+      else if(selection == 2) {
+        System.out.println("Apple, Banana, Coconut");
+      }
+      else if(selection == 3){
+        System.out.println("Goodbye");
+        //bool = false;
+        break;
+      }
+    }
+    scanner.close();
+  }
+}
